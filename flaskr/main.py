d4359bd77d734a5113d59b417eb06442643b19b5
Matthew Bacon <mcb9sm@virginia.edu>
2019-03-02 17:41:22 -0500

    Edited main.py so it can upload files

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,6 +1,8 @@
-from flask import Flask,render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_pymongo import PyMongo
+import sys
 import uuid
+
 app = Flask(__name__)
 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
@@ -10,18 +12,25 @@ mongo = PyMongo(app)
 @app.route("/")
 def returnUniqueString():
 	#GENERATE UNIQUE ID
-	return str(uuid.uuid4())
+	return '''localhost:5000/user/''' + str(uuid.uuid4())
 
 @app.route("/user/<id>")
 def connect(id):
-	return render_template("index.html")
+	return render_template("index.html", id=id)
 
-@app.route("/upload/<id>",methods=["POST"])
+@app.route("/upload/<id>", methods=["POST"])
 def upload(id):
-	file = request.files["file"]
-	mongo.save_file(id+str(uuid.uuid4()),file)
-	return "uploaded"
-	
+	app.logger.info("HELLO")
+	f = request.files['file']
+
+	if f:
+		print(f, file=sys.stderr)
+	else:
+		print("FUCK", file=sys.stderr)
+	# mongo.save_file(id+str(uuid.uuid4()),file)
+	# return "uploaded"
+	return redirect(url_for('connect', id=id))
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
