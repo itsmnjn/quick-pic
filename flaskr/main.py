173a88a38a82282d179c4b4188e7b0ee71ca1841
Matthew Bacon <mcb9sm@virginia.edu>
2019-03-02 14:32:42 -0500

    edited function returnUniqueString

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template
 from flask_pymongo import PyMongo
+import uuid
 app = Flask(__name__)
 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
@@ -8,7 +9,7 @@ mongo = PyMongo(app)
 @app.route("/")
 def returnUniqueString():
 	#GENERATE UNIQUE ID
-	return "adfjkladsjfkladsjfkldasjklfjsd"
+	return str(uuid.uuid4())
 
 @app.route("/user/<id>")
 def connect(id):
