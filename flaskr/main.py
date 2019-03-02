6cadcaad891197daf877a4ccffc36c885b68f318
Matthew Bacon <mcb9sm@virginia.edu>
2019-03-02 15:15:31 -0500

    Edited main.py

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,9 +1,10 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 from flask_pymongo import PyMongo
 import uuid
 app = Flask(__name__)
 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+app.config['MAX_CONTENT_LENGTH'] = 16*1024*1024
 mongo = PyMongo(app)
 
 @app.route("/")
@@ -15,10 +16,11 @@ def returnUniqueString():
 def connect(id):
 	return render_template("index.html")
 
-@app.route("/upload/<id>")
+@app.route("/upload/<id>",methods=["POST"])
 def upload(id):
 	file = request.files["inputImages"]
-	mongo.save_file(id+"1",
+	return file.filename
+	mongo.save_file(id+str(uuid.uuid4()),file)
 
 if __name__ == "__main__":
 	app.run(debug=True)
