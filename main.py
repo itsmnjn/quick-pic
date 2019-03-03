58aec7b1f7197382c07dc3e57d3758145bcf6b42
devericx <devericx@gmail.com>
2019-03-03 03:28:49 -0500

    Upload to specific database 'ids'

--- main.py
+++ main.py
@@ -1,52 +1,58 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, make_response
 from flask_pymongo import PyMongo
 import sys
 import uuid
 import dns
+import mimetypes
 
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'insta-doc-pic'
-app.config["MONGO_URI"] = "mongodb+srv://Admin:Password@insta-doc-pic-czey7.gcp.mongodb.net/test?retryWrites=true"
-#app.config['MAX_CONTENT_LENGTH'] = 16*1024*1024
+app.config["MONGO_URI"] = "mongodb://Admin:Password@insta-doc-pic-shard-00-00-czey7.gcp.mongodb.net:27017,insta-doc-pic-shard-00-01-czey7.gcp.mongodb.net:27017,insta-doc-pic-shard-00-02-czey7.gcp.mongodb.net:27017/test?ssl=true&replicaSet=insta-doc-pic-shard-0&authSource=admin&retryWrites=true"
+
 mongo = PyMongo(app)
-#client = MongoClient('localhost', 27017)
+db = mongo.cx['ids']
 
 @app.route("/")
 def returnUniqueString():
-	#GENERATE UNIQUE ID
-	id = str(uuid.uuid4())
-	return id
+    #GENERATE UNIQUE ID
+    id = str(uuid.uuid4())
+    resp = make_response(id)
+    resp.headers["Access-Control-Allow-Origin"] = "*"
+    return resp
 
 @app.route("/user/<id>")
 def connect(id):
-	coll = mongo.db[id]
-	return render_template("index.html", id=id)
+    resp = make_response(render_template("index.html", id=id))
+    return resp
 
 @app.route("/upload/<id>", methods=["POST"])
 def upload(id):
-	coll = mongo.db[id]
-	app.logger.info("HELLO")
-	f = request.files['file']
-	coll.insert({'name':f.filename, 'data':f.read()})
-	print("uploaded")
-	return redirect(url_for('connect', id=id))
+    coll = db[id]
+    app.logger.info("HELLO")
+
+    response = request.files['file']
+    image = response.read()
+
+    coll.insert_one({ 'name': response.filename, 'data': image })
+    return redirect(url_for('connect', id=id))
 
 @app.route("/retrieve/<id>")
 def getUpload(id):
-	coll = mongo.db[id]
-	f= coll.find();
-	numDeleted  = coll.delete_many({})
-	total="";
-	for imgs in f:
-		return imgs["data"]
-	#f= mongo.send_file(id)
-	#return total
+    coll = db[id]
+    f = coll.find_one_and_delete({})
 
-@app.route("/delete/<id>")
-def delete(id):
-	coll = mongo.db[id]
-	numDeleted  = coll.delete_many({})
-	return "deleted " + str(numDeleted.deleted_count)
+    if not f:
+        return ""
+    
+    contentType = mimetypes.guess_type(f["name"])
+
+    if not contentType[0]:
+        contentType[0] = "image/jpeg"
+    
+    resp = make_response(f["data"])
+    resp.headers["Content-Type"] = contentType[0]
+    
+    return resp
 
 if __name__ == "__main__":
-	app.run(debug=True, port=8080)
+    app.run(debug=True, port=8080)
