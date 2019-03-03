9a72a609532416f0b0b9ad630c5c649098969c02
Matthew Bacon <mcb9sm@virginia.edu>
2019-03-02 19:15:43 -0500

    connected to mongo database

--- flaskr/main.py
+++ flaskr/main.py
@@ -2,12 +2,14 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_pymongo import PyMongo
 import sys
 import uuid
+import dns
 
 app = Flask(__name__)
-
-app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
-app.config['MAX_CONTENT_LENGTH'] = 16*1024*1024
+app.config['MONGO_DBNAME'] = 'insta-doc-pic'
+app.config["MONGO_URI"] = "mongodb+srv://Admin:Password@insta-doc-pic-czey7.gcp.mongodb.net/test?retryWrites=true"
+#app.config['MAX_CONTENT_LENGTH'] = 16*1024*1024
 mongo = PyMongo(app)
+#client = MongoClient('localhost', 27017)
 
 @app.route("/")
 def returnUniqueString():
@@ -27,10 +29,13 @@ def upload(id):
 		print(f, file=sys.stderr)
 	else:
 		print("FUCK", file=sys.stderr)
-	# mongo.save_file(id+str(uuid.uuid4()),file)
-	# return "uploaded"
+	mongo.save_file(id, f)
+	print( "uploaded")
 	return redirect(url_for('connect', id=id))
 
+@app.route("/retrieve/<id>")
+def getUpload(id):
+	return mongo.send_file(id)
 
 if __name__ == "__main__":
 	app.run(debug=True)
